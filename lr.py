d6841416bbc264f82d23ea4082a4e6355da85b35
ilmir22 <152898248+ilmir22@users.noreply.github.com>
2024-01-25 20:21:34 +0300

    Add files via upload

---
+++ lr.py
@@ -0,0 +1,53 @@
+# text_1 = "some text"
+
+# for i in text_1:
+#     print(i)
+
+# text_1 = "123456"
+
+# print(text_1[0])
+# print(text_1[-1])
+# print(text_1[::-1])
+
+# text_1 = "123456"
+
+# text_2 = "7895"
+
+# print(text_1 + text_2)
+
+
+# serLogin=input("Your login: ") 
+# strMsg="Welcome, {}. Let's start game!".format(userLogin)
+# print(strMsg)
+
+
+# import string 
+
+# a = ","
+
+# text = "Hello, there, how are you?"
+
+# print(f"count of punctuation {text.count('a')}")
+
+
+# my_number = 0 
+# my_list = [0, 1, 2, "text", 8, my_number]
+
+# for i in my_list:
+#     print(i)
+
+# from random import randint
+
+# gen_list = [randint(-10, 10) for _ in range(10)]
+# print(gen_list)
+# print(len(gen_list))
+# gen_list[0] = 100
+# print(gen_list)
+# gen_list.insert(2, 1000)
+# print(gen_list)
+# print(max(gen_list))
+# print(120 in gen_list)
+# print(gen_list.index(100))
+
+
+
